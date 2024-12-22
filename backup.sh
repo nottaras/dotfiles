aec44fe4d94ac3aa0279d89eb04596d4d6ae86f3
Taras Zadziarnouski <taras.zadziarnouski@gmail.com>
2024-12-22 18:50:47 +0100

    add backup.sh

---
+++ backup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Backup Homebrew
+mkdir -p homebrew
+brew list --formula > homebrew/brew-formulas.txt
+brew list --cask > homebrew/brew-casks.txt
+
+# Backup Zsh
+mkdir -p zsh
+cp ~/.zshrc zsh/.zshrc
+cp ~/.p10k.zsh zsh/.p10k.zsh
+
+# Backup Git
+mkdir -p git
+cp -r ~/.ssh git/.ssh
+cp ~/.gitconfig git/.gitconfig
+cp ~/.gitconfig-ffp git/.gitconfig-ffp
+
+# Backup VSCode
+mkdir -p vscode
+cp /Users/taras/Library/Application\ Support/Code/User/settings.json vscode/settings.json
+cp /Users/taras/Library/Application\ Support/Code/User/keybindings.json vscode/keybindings.json
+cp -r /Users/taras/Library/Application\ Support/Code/User/snippets vscode/snippets
+
+# Backup IntelliJ Idea
+
+# Backup Obsidian
+mkdir -p obsidian
+rsync -av --exclude 'workspace.json' --exclude 'workspace-mobile.json' /Users/taras/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/Vault/.obsidian .
