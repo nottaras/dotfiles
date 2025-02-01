9f819d88d7073e2ee21a586a7728295e7c67dca0
Taras Zadziarnouski <taras.zadziarnouski@gmail.com>
2025-02-01 15:13:04 +0100

    update

--- backup.sh
+++ backup.sh
@@ -9,11 +9,3 @@ brew list --cask > homebrew/brew-casks.txt
 mkdir -p zsh
 cp ~/.zshrc zsh/.zshrc
 cp ~/.p10k.zsh zsh/.p10k.zsh
-
-# Backup VSCode
-mkdir -p vscode
-cp /Users/taras/Library/Application\ Support/Code/User/settings.json vscode/settings.json
-cp /Users/taras/Library/Application\ Support/Code/User/keybindings.json vscode/keybindings.json
-cp -r /Users/taras/Library/Application\ Support/Code/User/snippets vscode/snippets
-
-# Backup IntelliJ Idea
